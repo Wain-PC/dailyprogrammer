d9a2c19b3a0554871abf0fd120a2c50cf5241c24
Wain-PC <wain-pc@mail.ru>
2018-04-08 00:22:23 +0300

    -Challenge #355.2

---
+++ 355/355.2.js
@@ -0,0 +1,25 @@
+const solve = function (pumpkin, apples, eggs, milk, sugar) {
+    const ingredients = {pumpkin, apples, eggs, milk, sugar};
+    let max = 0;
+    //Simple `check all possible combinations` strategy`, save a maximum total value and return it.
+    for(let i = 0, a = bakeN(applePie, ingredients, i); canBake(a); i++, a = bakeN(applePie, ingredients, i)) {
+        for(let j = 0, b = bakeN(pumpkinPie, a, j); canBake(b); j++, b = bakeN(pumpkinPie, a, j)) {
+            const current = i + j;
+            if(current > max) {
+                max = current;
+            }
+        }
+    }
+    return max;
+};
+
+const pumpkinPie = ({pumpkin, apples, eggs, milk, sugar}, n = 1) => ({pumpkin: pumpkin - n, apples, eggs: eggs - 3*n, milk: milk - 4*n, sugar: sugar - 3*n});
+const applePie = ({pumpkin, apples, eggs, milk, sugar}, n = 1) => ({pumpkin, apples: apples - n, eggs: eggs - 4*n, milk: milk - 3*n, sugar: sugar - 2*n});
+const canBake = (ingredients) => Object.keys(ingredients).every(i => ingredients[i] >= 0);
+const bakeN = (cookFn, ingredients, n) => cookFn(ingredients, n);
+
+
+
+
+
+module.exports = solve;
