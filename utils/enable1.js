6e152bb11e72fedd0c94fdd1ab886f1581529766
Wain-PC <wain-pc@mail.ru>
2018-11-10 21:26:30 +0300

    -Added a set of utils for `enable1` word list

---
+++ utils/enable1.js
@@ -0,0 +1,48 @@
+const { readFile } = require('fs');
+
+const url = './utils/enable1.txt';
+
+const string = () => new Promise((resolve, reject) => {
+  readFile(url, (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(data.toString('utf-8'));
+  });
+});
+
+const array = async () => {
+  const data = await string();
+  return data.split(/\r\n/);
+};
+const object = async () => {
+  const data = await array();
+  return data.reduce((obj, str) => {
+    obj[str] = true; // eslint-disable-line no-param-reassign
+    return obj;
+  }, {});
+};
+
+const tree = async () => {
+  const fn = (str, obj) => {
+    if (!str.length) {
+      obj.end = true; // eslint-disable-line no-param-reassign
+      return;
+    }
+    const letter = str[0];
+    if (!obj[letter]) {
+      obj[letter] = {}; // eslint-disable-line no-param-reassign
+    }
+    fn(str.substr(1), obj[letter]);
+  };
+  const data = await array();
+  return data.reduce((obj, str) => {
+    fn(str, obj); // eslint-disable-line no-param-reassign
+    return obj;
+  }, {});
+};
+
+module.exports = {
+  string, array, object, tree,
+};
