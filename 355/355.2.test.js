30a1022608c1e1e13b035119e9758a30993f1487
Wain-PC <wain-pc@mail.ru>
2018-11-10 19:53:43 +0300

    -Enforced AirBnB ESlint code style on current files

--- 355/355.2.test.js
+++ 355/355.2.test.js
@@ -1,13 +1,13 @@
 const solve = require('./355.2');
 
-describe('355.2', ()=> {
-    it('should pass example 1', ()=> {
-        expect(solve(10,14,10,42,24)).toBe(3);
-    });
-    it('should pass example 2', ()=> {
-        expect(solve(12,4,40,30,40)).toBe(8);
-    });
-    it('should pass example 3', ()=> {
-        expect(solve(12,14,20,42,24)).toBe(6);
-    });
+describe('355.2', () => {
+  it('should pass example 1', () => {
+    expect(solve(10, 14, 10, 42, 24)).toBe(3);
+  });
+  it('should pass example 2', () => {
+    expect(solve(12, 4, 40, 30, 40)).toBe(8);
+  });
+  it('should pass example 3', () => {
+    expect(solve(12, 14, 20, 42, 24)).toBe(6);
+  });
 });
