b2b82e26ebb6d6c3c5ba5350bb62a0b94b7a0470
Wain-PC <wain-pc@mail.ru>
2018-11-10 22:07:27 +0300

    -Task 366.1 + Bonuses

--- 366/366.1.js
+++ 366/366.1.js
@@ -1,3 +1,5 @@
+const { object, array } = require('../utils/enable1');
+
 const funnel = (input, expected) => {
   const { length } = input;
   if (length !== expected.length + 1) {
@@ -6,4 +8,31 @@ const funnel = (input, expected) => {
   return input.split('').some((letter, i) => input.substr(0, i) + input.substr(i + 1, length) === expected);
 };
 
-module.exports = funnel;
+const bonus = async (input, obj) => {
+  const dataObj = obj || await object();
+  return input.split('').reduce((arr, letter, i) => {
+    const newWord = input.substr(0, i) + input.substr(i + 1, input.length);
+    if (dataObj[newWord] && !arr.includes(newWord)) {
+      arr.push(newWord);
+    }
+    return arr;
+  }, []);
+};
+const bonus2 = async () => {
+  const outArr = [];
+  const promiseArr = [];
+  const dataArr = await array();
+  const dataObj = await object();
+
+  dataArr.forEach((word) => {
+    if (word.length < 5) {
+      return;
+    }
+    promiseArr.push(bonus(word, dataObj).then(words => (words.length === 5 ? word : null)));
+  });
+
+  const wordsArr = await Promise.all(promiseArr);
+  return wordsArr.filter(word => word);
+};
+
+module.exports = { funnel, bonus, bonus2 };
--- 366/366.1.test.js
+++ 366/366.1.test.js
@@ -1,4 +1,4 @@
-const funnel = require('./366.1');
+const { funnel, bonus, bonus2 } = require('./366.1');
 
 describe('366.1', () => {
   it('should pass examples', () => {
@@ -8,5 +8,43 @@ describe('366.1', () => {
     expect(funnel('eave', 'leave')).toBe(false);
     expect(funnel('sleet', 'lets')).toBe(false);
     expect(funnel('skiff', 'ski')).toBe(false);
+  });
+  it('should pass bonus examples', async () => {
+    expect(await bonus('dragoon')).toEqual(['dragon']);
+    expect(await bonus('boats')).toEqual(['oats', 'bats', 'bots', 'boas', 'boat']);
+    expect(await bonus('affidavit')).toEqual([]);
+  });
+  it('should pass bonus #2', async () => {
+    const wordsWithFiveFunnels = await bonus2();
+    expect(wordsWithFiveFunnels.length).toEqual(28);
+    expect(wordsWithFiveFunnels).toEqual([
+      'beasts',
+      'boats',
+      'brands',
+      'chards',
+      'charts',
+      'clamps',
+      'coasts',
+      'cramps',
+      'drivers',
+      'grabblers',
+      'grains',
+      'grippers',
+      'moats',
+      'peats',
+      'plaints',
+      'rousters',
+      'shoots',
+      'skites',
+      'spates',
+      'spicks',
+      'spikes',
+      'spines',
+      'teats',
+      'tramps',
+      'twanglers',
+      'waivers',
+      'writes',
+      'yearns']);
   });
 });
