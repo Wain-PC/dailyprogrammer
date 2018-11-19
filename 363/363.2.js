95186afeaabb2f68df615ef840c9ee85b36869b7
Pavel Dranichnikov <Pavel.Dranichnikov@kaspersky.com>
2018-11-19 18:32:01 +0300

    -Task 363.2

--- 363/363.2.js
+++ 363/363.2.js
@@ -1,20 +1,56 @@
 const { EOL } = require('os');
 const { get } = require('../utils/file');
 
+const getPatterns = async () => (await get('./363/patterns.txt'))
+  .split(EOL)
+  .filter(p => p)
+  .map((pattern) => {
+    const starts = pattern.indexOf('.') === 0;
+    const ends = pattern.indexOf('.') === pattern.length - 1;
+    const hyphens = [];
+    const text = pattern
+      .replace(/\./g, '')
+      .replace(/\d+?/g, (n, o) => {
+        hyphens.push({ weight: +n, offset: o - hyphens.length - 1 });
+        return '';
+      });
+    return {
+      starts, ends, text, hyphens,
+    };
+  });
+
 const solve = async (input) => {
-  const patterns = (await get('./363/patterns.txt')).split(EOL);
-  console.log(patterns);
+  const patterns = await getPatterns();
   const weights = Array(input.length - 1).fill(0);
-  patterns.forEach((pattern) => {
-    // TODO: Apply each pattern to the input.
-    // If the patterns fills, its numbers should be put to `weights` array
-    // (only if the current weight on the corresponding position is less than ours
+  patterns.forEach(({
+    starts, ends, text, hyphens,
+  }) => {
+    const re = new RegExp(text, 'gi');
+    let match = re.exec(input);
+    while (match) {
+      const { index } = match;
+      if (index === -1) {
+        return;
+      }
+      if (
+        (starts && index === 0)
+        || (ends && index === input.length - text.length)
+        || (!starts && !ends)) {
+        hyphens.forEach(({ weight, offset }) => {
+          const weightOffset = index + offset;
+          if (weights[weightOffset] < weight) {
+            weights[weightOffset] = weight;
+          }
+        });
+      }
+      match = re.exec(input);
+    }
   });
 
   // Construct output string
   return input.split('').reduce((arr, letter, index) => {
     arr.push(letter);
-    if (weights[index]) {
+    if (weights[index] % 2 === 1) {
       arr.push('-');
     }
     return arr;
--- 363/363.2.test.js
+++ 363/363.2.test.js
@@ -2,20 +2,21 @@ const { solve } = require('./363.2');
 
 describe('363.2', () => {
   const examples = [
+    ['buttons', 'but-ton-s'],
+    ['bewildering', 'be-wil-der-ing'],
+    ['programmer', 'pro-gram-mer'],
     ['mistranslate', 'mis-trans-late'],
     ['alphabetical', 'al-pha-bet-i-cal'],
-    ['bewildering', 'be-wil-der-ing'],
-    ['buttons', 'but-ton-s'],
     ['ceremony', 'cer-e-mo-ny'],
     ['hovercraft', 'hov-er-craft'],
     ['lexicographically', 'lex-i-co-graph-i-cal-ly'],
-    ['programmer', 'pro-gram-mer'],
     ['recursion', 're-cur-sion'],
   ];
 
   examples.forEach(([input, expectedResult]) => {
     it(`should solve main task (input '${input}')`, async () => {
       const result = await solve(input);
+      console.log(input, result);
       expect(result).toBe(expectedResult);
     });
   });
