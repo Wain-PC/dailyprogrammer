f892568b90d939cb2c3f04120f31cbaa1fb30a45
Wain-PC <wain-pc@mail.ru>
2018-12-06 19:27:42 +0300

    -Task 360.2 bonus

--- 360/360.2.js
+++ 360/360.2.js
@@ -4,6 +4,19 @@ const euclidianDistance = (lat1 = 0, lon1 = 0, lat2 = 0, lon2 = 0) => Math.sqrt(
   ((lat2 - lat1) ** 2) + ((lon2 - lon1) ** 2),
 );
 
+const rad = x => x * Math.PI / 180;
+
+const geodesicDistance = (lat1 = 0, lon1 = 0, lat2 = 0, lon2 = 0) => {
+  const earthRadius = 6371; // in kilometers
+  const lat1Angle = rad(lat1);
+  const lat2Angle = rad(lat2);
+  const latAngle = lat2Angle - lat1Angle;
+  const lonAngle = rad(lon2 - lon1);
+  const a = (Math.sin(latAngle / 2) ** 2)
+    + (Math.cos(lat1Angle) * Math.cos(lat2Angle) * (Math.sin(lonAngle) ** 2));
+  return earthRadius * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+};
+
 const convertCoords = (latString, lonString) => {
   const latRegExp = /(\d{1,2}.\d{4}) ([NS])/;
   const lonRegExp = /(\d{1,2}.\d{4}) ([EW])/;
@@ -17,11 +30,15 @@ const convertCoords = (latString, lonString) => {
   return { lat, lon };
 };
 
-
-const solve = async (latString, lonString) => {
-  const { lat, lon } = convertCoords(latString, lonString);
+const getData = async () => {
   const data = await request.get('https://opensky-network.org/api/states/all');
   const { states: flights } = JSON.parse(data);
+  return flights;
+};
+
+
+const solve = async (flights, latString, lonString, distanceCalculator = euclidianDistance) => {
+  const { lat, lon } = convertCoords(latString, lonString);
   let closestFlight = {
     distance: Infinity,
   };
@@ -29,7 +46,7 @@ const solve = async (latString, lonString) => {
     if (onGround) {
       return;
     }
-    const distance = euclidianDistance(lat, lon, latitude, longitude);
+    const distance = distanceCalculator(lat, lon, latitude, longitude);
     if (distance < closestFlight.distance) {
       closestFlight = {
         distance,
@@ -46,4 +63,6 @@ const solve = async (latString, lonString) => {
   return closestFlight;
 };
 
-module.exports = { solve };
+const bonus = (...args) => solve(...args, geodesicDistance);
+
+module.exports = { getData, solve, bonus };
--- 360/360.2.test.js
+++ 360/360.2.test.js
@@ -1,4 +1,4 @@
-const { solve } = require('./360.2');
+const { solve, bonus, getData } = require('./360.2');
 
 
 describe('360.2', () => {
@@ -6,6 +6,11 @@ describe('360.2', () => {
     ['48.8584 N', '2.2945 E'],
     ['40.6413 N', '73.7781 W'],
   ];
+
+  let flights;
+  beforeAll(async () => {
+    flights = await getData();
+  });
 
   examples.forEach(([lat, lon]) => {
     it(`should solve main task (input '${lat} : ${lon}')`, async () => {
@@ -17,7 +22,7 @@ describe('360.2', () => {
         altitude,
         country,
         icao,
-      } = await solve(lat, lon);
+      } = await solve(flights, lat, lon);
 
       expect(distance).toBeGreaterThan(0);
       expect(typeof callsign).toBe('string');
@@ -28,6 +33,35 @@ describe('360.2', () => {
       expect(altitude).toBeGreaterThan(0);
       expect(typeof country).toBe('string');
       expect(typeof icao).toBe('string');
-    });
+    }, 20000);
+
+    it(`should solve bonus (input '${lat} : ${lon}')`, async () => {
+      const {
+        distance,
+        callsign,
+        latitude,
+        longitude,
+        altitude,
+        country,
+        icao,
+      } = await bonus(flights, lat, lon);
+
+      expect(distance).toBeGreaterThan(0);
+      expect(typeof callsign).toBe('string');
+      expect(latitude).toBeGreaterThanOrEqual(-90);
+      expect(latitude).toBeLessThanOrEqual(90);
+      expect(longitude).toBeGreaterThanOrEqual(-180);
+      expect(longitude).toBeLessThanOrEqual(180);
+      expect(altitude).toBeGreaterThan(0);
+      expect(typeof country).toBe('string');
+      expect(typeof icao).toBe('string');
+    }, 20000);
+
+    it(`'bonus' and 'solve' outputs should be the same (input '${lat} : ${lon}')`, async () => {
+      const { distance: distanceSolve, ...solveData } = await solve(flights, lat, lon);
+      const { distance: distanceBonus, ...bonusData } = await bonus(flights, lat, lon);
+
+      expect(solveData).toEqual(bonusData);
+    }, 20000);
   });
 });
