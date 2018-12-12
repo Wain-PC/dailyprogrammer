af12b64ec0ac59e84e928f19248723564e11154c
Wain-PC <wain-pc@mail.ru>
2018-12-12 16:51:46 +0300

    -Task 368.2 + bonus (solves N=10 in less than 2 minutes). This can probably be optimized in a number of ways, but it works...

--- 368/368.2.js
+++ 368/368.2.js
@@ -1,14 +1,3 @@
-const randomMatrix = (size) => {
-  const matrix = [];
-  for (let i = 0; i < size; i++) {
-    matrix[i] = [];
-    for (let j = 0; j < size; j++) {
-      matrix[i][j] = Math.round(Math.random());
-    }
-  }
-  return matrix;
-};
-
 const check = (matrix) => {
   const { length } = matrix;
   for (let i = 0; i < length - 1; i++) {
@@ -16,13 +5,12 @@ const check = (matrix) => {
       const topLeft = matrix[i][j];
       for (let ii = i + 1; ii < length; ii++) {
         const bottomLeft = matrix[ii][j];
-        for (let jj = j + 1; jj < length; jj++) {
-          const topRight = matrix[i][jj];
-          const bottomRight = matrix[ii][jj];
-          const sum = topLeft + bottomLeft + topRight + bottomRight;
-          if (sum === 0 || sum === 4) {
-            return false;
-          }
+        const jj = ii - i + j;
+        const topRight = matrix[i][jj];
+        const bottomRight = matrix[ii][jj];
+        const sum = topLeft + bottomLeft + topRight + bottomRight;
+        if (sum === 0 || sum === 4) {
+          return false;
         }
       }
     }
@@ -33,14 +21,35 @@ const check = (matrix) => {
 // eslint-disable-next-line no-console
 const print = matrix => console.log(matrix.reduce((out, row) => `${out}${row.join(' ')}\n`, ''));
 
-const solve = (n) => {
-  let matrix;
-  let result = false;
-  do {
-    matrix = randomMatrix(n);
-    result = check(matrix);
-  } while (!result);
-  return matrix;
+const padLeft = (s, l, c) => c.repeat(l - s.length) + s;
+
+const run = (matrix, n) => {
+  const { length } = matrix;
+  // Actual exit condition.
+  if (length === n) {
+    return matrix;
+  }
+  const desiredLength = length + 1;
+  const totalLength = 2 * desiredLength - 1;
+  const permsLength = 2 ** totalLength;
+  for (let i = 0; i < permsLength; i++) {
+    const binStr = padLeft(i.toString(2), totalLength, '0').split('').map(c => +c);
+    const col = binStr.slice(0, desiredLength - 1);
+    const row = binStr.slice(desiredLength - 1);
+    const newMatrix = matrix.map((r, index) => ([col[index], ...r]));
+    newMatrix.unshift(row);
+    if (check(newMatrix)) {
+      const result = run(newMatrix, n);
+      if (result) {
+        return result;
+      }
+    }
+  }
+  return false;
 };
 
-module.exports = { solve, check, print };
+const solve = n => run([], n);
+
+module.exports = {
+  solve, check, print,
+};
--- 368/368.2.test.js
+++ 368/368.2.test.js
@@ -1,21 +1,11 @@
 const { solve, check, print } = require('./368.2');
 
 describe('368.2', () => {
-  it('should solve a matrix with size = 2', () => {
-    const matrix = solve(2);
-    expect(check(matrix)).toBe(true);
-    print(matrix);
-  });
-
-  it('should solve a matrix with size = 3', () => {
-    const matrix = solve(3);
-    expect(check(matrix)).toBe(true);
-    print(matrix);
-  });
-
-  it('should solve a matrix with size = 4', () => {
-    const matrix = solve(4);
-    expect(check(matrix)).toBe(true);
-    print(matrix);
-  });
+  for (let i = 1; i <= 10; i++) {
+    it(`should solve a matrix with size = ${i}`, () => {
+      const matrix = solve(i);
+      expect(check(matrix)).toBe(true);
+      print(matrix);
+    });
+  }
 });
